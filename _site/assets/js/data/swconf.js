11edf50b8742882e5a44a70cf7a1f47838d13651
BeatrizMarques14 <marques.beatriz14@ua.pt>
2024-10-07 17:42:34 +0100

    tentaiva de resolução favicon

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728318170',resources: [
+    cacheName: 'chirpy-1728319123',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
